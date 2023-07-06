2be0deb5409ae209c398c2d3737630e58e41d98b
원일 허 <70367717+wonil-programmer@users.noreply.github.com>
2023-07-06 11:02:57 +0900

    [연습]부분집합(파이썬)

---
+++ DFS/BFS/부분집합.py
@@ -0,0 +1,14 @@
+n = int(input())
+check = [0] * (n + 1)
+def dfs(v):
+    if v == n + 1:
+        for i in range(1, n + 1):
+            if check[i] == 1:
+                print(i, end = ' ')
+        print()
+    else:
+        check[v] = 1
+        dfs(v + 1)
+        check[v] = 0
+        dfs(v + 1)
+dfs(1)
