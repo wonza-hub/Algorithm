f20e24058dbffcd4a607bc81d835441504328f16
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-07-17 20:46:37 +0900

    [연습]동전 분배하기(파이썬)

---
+++ DFS/BFS/동전분배하기.py
@@ -0,0 +1,22 @@
+n = int(input())
+sum = [0] * 3
+min_diff = 10e9
+coins = []
+for _ in range(n):
+    coins.appned(int(input()))
+
+def dfs(l):
+    global min_diff
+    global sum
+    if l == n:
+        if len(set(sum)) == 3:
+            min_diff = min(min_diff, max(sum) - min(sum))
+        return
+    else:
+        for i in range(len(sum)):
+            sum[i] += coins[l]
+            dfs(l + 1)
+            sum[i] -= coins[l]
+
+dfs(0)
+print(min_diff)
