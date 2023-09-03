e6a4ec8381f8b83d50be78f4e38d0a32941d9605
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2023-09-03 11:09:25 +0900

    [연습]대표값(파이썬)

---
+++ 구현/대표값.py
@@ -0,0 +1,19 @@
+n = int(input())
+scores = list(map(int, input().split()))
+avg = round(sum(scores) / n) 
+min_diff = 10e9
+ans = 1
+
+for idx, score in enumerate(scores):
+    diff = abs(score - avg)
+    if diff < min_diff:
+        min_diff = diff
+        closet_score = score
+        ans = idx + 1
+    elif diff == min_diff:
+        if score > closet_score:
+            closet_score = score
+            ans = idx + 1
+
+print(avg, ans, end=" ")
+        
