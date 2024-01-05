622914cde4d9fcc40dc362b5365bc81892422b3c
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-05 13:29:58 +0900

    [백준] 랜선 자르기

--- BinarySearch/랜선자르기.py
+++ BinarySearch/랜선자르기.py
@@ -1,4 +1,29 @@
 k, n = map(int, input().split())
+ans = 0
+lans = []
+for _ in range(k):
+    lan = int(input())
+    lans.append(lan)
+
+l, r = 1, max(lans) # l을 0으로 설정하면 아래의 코드에서 mid로 나누는 부분에서 ZeroDivisionError 발생
+
+while l <= r:
+    mid = (l + r) // 2
+    cnt = 0
+    for lan in lans:
+        tmp = lan // mid 
+        cnt += tmp
+        
+    if cnt >= n:
+        ans = mid
+        l = mid + 1
+    else:
+        r = mid - 1
+
+print(ans)
+    
+# 강의 풀이
+k, n = map(int, input().split())
 lans = []
 ans = 0
 
