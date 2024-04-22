a63fc355070d4c946d11c46263055025707d4ad9
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-04-22 22:04:09 +0900

    (복습) [백준] 영화감독숌

--- 탐색/완전탐색/영화감독숌[X].py
+++ 탐색/완전탐색/영화감독숌[X].py
@@ -13,4 +13,18 @@ while True:
         ans += 1
 
 print(ans)
+
+# 복습시 풀이
+n = int(input())
+series = 0
+title = 666
+
+while True:
+    if '666' in str(title):
+        series += 1
+    if series == n:
+        break
+    title += 1
+
+print(title)    
             
