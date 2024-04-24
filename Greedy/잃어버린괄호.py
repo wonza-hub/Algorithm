9333da7395937890ef9bd5b2e79686ff6fe9ea44
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-04-24 21:27:53 +0900

    (복습) [백준] 잃어버린 괄호

--- Greedy/잃어버린괄호.py
+++ Greedy/잃어버린괄호.py
@@ -16,3 +16,33 @@ for i in range(len(ops)):
     ans += operands[i]
 
 print(ans)
+
+# 복습 풀이
+nums = []
+optrs = []
+line = input() + '+'
+tmp = ''
+minus_flag = False
+
+answer = 0
+tmp_sum = 0
+
+for op in line:
+    if op == '-' or op == '+':
+        optrs.append(op)
+        num = int(tmp)
+        tmp_sum += num
+        nums.append(num)
+        tmp = ''
+    else:
+        tmp += op
+
+answer = sum(nums)
+for i in range(len(optrs)):
+    if optrs[i] == '-':
+        b = sum(nums[:i + 1])
+        answer = b - (tmp_sum - b)
+        break
+
+
+print(answer)
