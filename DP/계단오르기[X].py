d79fa7e1465736bb6a3819de52fe7b8e2583b835
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-05-05 14:08:00 +0900

    (복습) [백준] 계단오르기

--- DP/계단오르기[X].py
+++ DP/계단오르기[X].py
@@ -9,4 +9,22 @@ dp[2] = stair[1] + stair[2]
 for i in range(3, n + 1):
     dp[i] = max(dp[i - 3] + stair[i - 1] + stair[i], dp[i - 2] + stair[i])
 print(dp[n])
+
+# 복습 풀이
+n = int(input())
+s = [0] * 301
+d = [0] * 301
+
+for i in range(1, n + 1):
+    s[i] = int(input())
+
+d[1] = s[1]
+d[2] = s[1] + s[2]
+
+for j in range(3, n + 1):
+    d[j] = max(d[j - 3] + s[j - 1] + s[j], d[j - 2] + s[j])
+
+print(d[n])
+
+    
     
