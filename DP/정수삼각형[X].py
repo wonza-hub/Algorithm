6c3a5a3a44bfc5147fea8a2e20c0ee4c761ec9fa
wonza-hub <wonza8585@gmail.com>
2024-09-27 20:11:35 +0900

    (복습x2) [프로그래머스] 정수 삼각형

--- DP/정수삼각형[X].py
+++ DP/정수삼각형[X].py
@@ -40,3 +40,19 @@ for i in range(1, n):
         d[i][j] = d[i][j] + max(left, right)
         
 print(max(d[n - 1]))
+
+# 복습 풀이(2)
+#거쳐간 숫자의 합이 가장 큰 경우
+#대각선 방향으로 한 칸 오른쪽 또는 왼쪽으로만 이동 
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(i + 1):
+            a, b = 0, 0
+            if j != 0:
+                a = triangle[i - 1][j - 1]
+            if i != j:
+                b = triangle[i - 1][j]
+                
+            triangle[i][j] = max(a, b) + triangle[i][j]
+    
+    return max(triangle[len(triangle) - 1])
