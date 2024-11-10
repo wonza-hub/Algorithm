b1494dd4fa249c60330e24526c2488793193258c
wonza-hub <wonza8585@gmail.com>
2024-11-10 21:11:17 +0900

    [프로그래머스] 멀리 뛰기

---
+++ DP/멀리 뛰기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    # n=1 등 직접 초기화한 값 통과되는지 꼭 확인
+    if n==1: return 1;
+    if n==2: return 2;
+    dp=[0]*(n+1)
+    dp[1],dp[2]=1,2
+    for i in range(3,n+1):
+        dp[i]=dp[i-2]%1234567+dp[i-1]%1234567
+    
+    return dp[n]%1234567
