dd2957e1d0f0772dc0c08ccb9e558f06128ceb66
wonza-hub <wonza8585@gmail.com>
2025-01-17 21:27:39 +0900

    (복습) [백준] 숨바꼭질

--- BFS/숨바꼭질.py
+++ BFS/숨바꼭질.py
@@ -21,3 +21,29 @@ while q:
     if 0 <= cur_n - 1 < MAX_DIS and ch[cur_n - 1] == -1:
         ch[cur_n - 1] = ch[cur_n] + 1
         q.append(cur_n - 1)
+
+# 복습 풀이
+from collections import deque
+
+n,k=map(int,input().split())
+MAX_VAL=100001
+pos=[0]*MAX_VAL
+q=deque([])
+q.append(n)
+pos[n]=1
+
+while q:
+    x=q.popleft()
+    if x==k:
+        print(pos[x]-1)
+        break
+    if 0<=x-1<MAX_VAL and pos[x-1]==0:
+        pos[x-1]=pos[x]+1
+        q.append(x-1)
+    if 0<=x+1<MAX_VAL and pos[x+1]==0:
+        pos[x+1]=pos[x]+1
+        q.append(x+1)
+    if 0<=2*x<MAX_VAL and pos[2*x]==0:
+        pos[2*x]=pos[x]+1
+        q.append(2*x)
+    
