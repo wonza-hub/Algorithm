c3e8059430e58d31f121e85acfef00e615f61cdd
wonza-hub <wonza8585@gmail.com>
2025-01-17 13:41:07 +0900

    (복습) [백준] 토마토(7576)

--- BFS/토마토(7576).py
+++ BFS/토마토(7576).py
@@ -28,9 +28,42 @@ for i in matrix:
             exit(0)
     res = max(res, max(i))
 print(res - 1)
-    
-            
 
+# 복습 풀이
+from collections import deque
 
+m,n=map(int,input().split())
+tomatoes=[list(map(int,input().split())) for _ in range(n)]
 
-                
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
+ans=0
+empty_cnt=0
+
+q=deque()
+for i in range(n):
+    for j in range(m):
+        if tomatoes[i][j]==1:
+            q.append((i,j))
+        elif tomatoes[i][j]==0:
+            empty_cnt+=1
+
+def bfs():
+    global ans,empty_cnt
+    if empty_cnt==0:
+        return
+    while q:
+        x,y=q.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if 0<=nx<n and 0<=ny<m and tomatoes[nx][ny]==0:
+                empty_cnt-=1
+                tomatoes[nx][ny]=tomatoes[x][y]+1
+                ans=max(ans,tomatoes[nx][ny]-1)
+                q.append((nx,ny))
+    if empty_cnt:
+        ans=-1
+
+bfs()
+print(ans)
