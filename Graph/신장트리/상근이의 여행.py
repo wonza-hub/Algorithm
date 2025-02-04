635f0680d2b989af11064e626f4be98dc17ad619
wonza-hub <wonza8585@gmail.com>
2025-02-05 08:46:59 +0900

    [백준] 상근이의 여행

---
+++ Graph/신장트리/상근이의 여행.py
@@ -0,0 +1,9 @@
+import sys
+input=sys.stdin.readline
+
+for _ in range(int(input())):
+    n,m=map(int,input().rstrip().split())
+    for _ in range(m):
+        a,b=map(int,input().rstrip().split())
+    print(n-1)
+
