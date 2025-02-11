e2965b3a1ddebd10464e49d4a8be65401e051177
wonza-hub <wonza8585@gmail.com>
2025-02-11 22:40:04 +0900

    comment: 복습 풀이 표시 주석 추가

--- Graph/서로소집합(Disjoint-set)/[X]집합의표현.py
+++ Graph/서로소집합(Disjoint-set)/[X]집합의표현.py
@@ -1,38 +1,39 @@
-# import sys
-# sys.setrecursionlimit(10 ** 6)
-# input = sys.stdin.readline
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
 
-# n, m = map(int, input().split())
-# parent = [0] * (n + 1)
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
 
-# def find_parent(parent, x):
-#     if parent[x] != x:
-#         parent[x] = find_parent(parent, parent[x])
-#     return parent[x]
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
-# def union_parent(parent, a, b):
-#     a = find_parent(parent, a)
-#     b = find_parent(parent, b)
-#     if a == b:
-#         return
-#     if a < b:
-#         parent[b] = a
-#     else:
-#         parent[a] = b
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a == b:
+        return
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
-# for i in range(n + 1):
-#     parent[i] = i
+for i in range(n + 1):
+    parent[i] = i
 
-# for _ in range(m):
-#     calc, a, b = map(int, input().split())
-#     if calc == 0:
-#         union_parent(parent, a, b)
-#     elif calc == 1:
-#         if find_parent(parent, a) == find_parent(parent, b):
-#             print('YES')
-#         else:
-#             print('NO')
+for _ in range(m):
+    calc, a, b = map(int, input().split())
+    if calc == 0:
+        union_parent(parent, a, b)
+    elif calc == 1:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print('YES')
+        else:
+            print('NO')
 
+# 복습 풀이
 import sys
 sys.setrecursionlimit(10 ** 6)
 input=sys.stdin.readline
