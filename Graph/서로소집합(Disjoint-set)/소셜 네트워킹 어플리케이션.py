834a355ab7e6cbb3f426b05fffa3205518984fb3
wonza-hub <wonza8585@gmail.com>
2025-02-11 22:41:26 +0900

    [백준] 소셜 네트워킹 어플리케이션

---
+++ Graph/서로소집합(Disjoint-set)/소셜 네트워킹 어플리케이션.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+input=sys.stdin.readline
+
+def find(p,x):
+    if p[x]!=x:
+        p[x]=find(p,p[x])
+    return p[x]
+
+def uni(p,x,y):
+    if x==y:
+        return
+    x=find(p,x)
+    y=find(p,y)
+    p[x]=y
+
+for t in range(1,int(input().rstrip())+1):
+    print(f'Scenario {t}:')
+    n=int(input().rstrip())
+    p=[0]*(n+1)
+    for i in range(n+1):
+        p[i]=i
+
+    k=int(input().rstrip())
+    for _ in range(k):
+        # 두 친구 간 union 연산 수행
+        a,b=map(int,input().rstrip().split())
+        uni(p,a,b)
+
+    m=int(input().rstrip())
+    for _ in range(m):
+        # 두 친구 각각 find 연산 수행
+        u,v=map(int,input().rstrip().split())
+        if find(p,u)==find(p,v):
+            print(1)
+        else:
+            print(0)
+    print()
