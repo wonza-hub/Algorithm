461bc6d1fb1f44e4face8183bd6d89678e6365fb
wonza-hub <wonza8585@gmail.com>
2025-03-04 12:05:27 +0900

    (복습) [백준] 자두나무

--- DP/2차원/[X]자두나무.py
+++ DP/2차원/[X]자두나무.py
@@ -22,3 +22,25 @@ for j in range(w+1):
             arr[i][j]=max(arr[i-1][j-1], arr[i-1][j])
 
 print(max(arr[t]))
+
+# 복습 풀이
+import sys
+input=sys.stdin.readline
+
+t,w=map(int,input().rstrip().split())
+tree=[0]*(t+1)
+arr=[[0]*(w+1) for _ in range(t+1)]
+
+for i in range(1,t+1):
+    tree[i]=int(input().rstrip())
+
+for j in range(w+1):
+    for i in range(1,t+1):
+        # 현재 자두 위치
+        loc=1 if j%2==0 else 2
+        if j==0:
+            arr[i][0]+=arr[i-1][0]+int(loc==tree[i])
+        else:
+            arr[i][j]=max(arr[i-1][j-1],arr[i-1][j])+int(loc==tree[i])
+
+print(max(arr[t]))
