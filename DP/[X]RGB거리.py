65d05bd19237b2ce93d792fb367c85f60bfa0e48
wonza-hub <wonza8585@gmail.com>
2025-03-06 12:16:28 +0900

    (복습) [백준] RGB 거리

--- DP/[X]RGB거리.py
+++ DP/[X]RGB거리.py
@@ -13,3 +13,17 @@ for i in range(1,n):
     dp[i][1]=min(dp[i-1][0],dp[i-1][2])+cost[i][1]
     dp[i][2]=min(dp[i-1][0],dp[i-1][1])+cost[i][2]
 print(min(dp[n-1]))
+
+# 복습 풀이
+import sys
+input=sys.stdin.readline
+
+n=int(input().rstrip())
+a=[list(map(int,input().rstrip().split())) for _ in range(n)]
+
+for i in range(1,n):
+    a[i][0]+=min(a[i-1][1],a[i-1][2])
+    a[i][1]+=min(a[i-1][0],a[i-1][2])
+    a[i][2]+=min(a[i-1][1],a[i-1][0])
+
+print(min(*a[n-1]))
