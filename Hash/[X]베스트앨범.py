490d2f649aa0da64057cba571ecbe9bec11cf6e3
wonza-hub <wonza8585@gmail.com>
2025-03-10 18:26:51 +0900

    (복습) [프로그래머스] 베스트 앨범

--- Hash/[X]베스트앨범.py
+++ Hash/[X]베스트앨범.py
@@ -21,3 +21,22 @@ def solution(genres, plays):
                 cnt += 1
                 
     return answer
+
+# 복습 풀이
+from collections import defaultdict
+
+def solution(genres, plays):
+    d=dict()
+    gd=defaultdict(int)
+    for idx,genre,play in zip(range(len(genres)),genres,plays):
+        d[idx]=[genre,play]
+        gd[genre]+=play
+    d=dict(sorted(d.items(),key=lambda x:[-x[1][1],x[0]]))
+    gd=dict(sorted(gd.items(),key=lambda x:-x[1]))
+    
+    ans=[]
+    for g in gd.keys():
+        tmp=[k for k,[genre,_] in d.items() if genre==g]
+        ans+=tmp[:min(len(tmp),2)]
+    
+    return ans
