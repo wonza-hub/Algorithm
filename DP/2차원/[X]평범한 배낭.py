005c39c71a54c1fac062690e2dd18e2ec04c0265
wonza-hub <wonza8585@gmail.com>
2025-05-04 11:13:57 +0900

    (복습) [백준] 평범한 배낭

--- DP/2차원/[X]평범한 배낭.py
+++ DP/2차원/[X]평범한 배낭.py
@@ -18,3 +18,22 @@ for cur in range(1, n + 1):
 
 print(dp[-1][-1])
     
+
+# 복습 풀이
+import sys
+input=sys.stdin.readline
+
+n,k=map(int,input().rstrip().split())
+w,v=[0]*(n+1),[0]*(n+1)
+for i in range(1,n+1):
+    w[i],v[i]=map(int,input().rstrip().split())
+
+d=[[0]*(k+1) for _ in range(n+1)]
+for cur in range(1,n+1):
+    for limit in range(1,k+1):
+        if w[cur]>limit:
+            d[cur][limit]=d[cur-1][limit]
+        else:
+            d[cur][limit]=max(d[cur-1][limit],d[cur-1][limit-w[cur]]+v[cur])
+
+print(d[-1][-1])
