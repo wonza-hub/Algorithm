f2d9a3df2ed5fe82146ccdfe808ff3161d249979
wonza-hub <wonza8585@gmail.com>
2025-07-18 22:57:52 +0900

    (복습) [백준] 소셜 네트워킹 어플리케이션

--- Graph/서로소집합(Disjoint-set)/소셜 네트워킹 어플리케이션.py
+++ Graph/서로소집합(Disjoint-set)/소셜 네트워킹 어플리케이션.py
@@ -36,3 +36,34 @@ for t in range(1,int(input().rstrip())+1):
         else:
             print(0)
     print()
+
+# 복습 풀이 
+import sys
+input=sys.stdin.readline
+
+
+def find(x):
+    if p[x]!=x:
+        p[x]=find(p[x])
+    return p[x]
+
+def uni(x,y):
+    if x==y:
+        return
+    x=find(x)
+    y=find(y)
+    p[x]=y
+
+for t in range(1,int(input().rstrip())+1):
+    print(f'Scenario {t}:')
+    n=int(input().rstrip())
+    p=[i for i in range(n)]
+    for _ in range(int(input().rstrip())):
+        a,b=map(int,input().rstrip().split())
+        uni(a,b)
+    for _ in range(int(input().rstrip())):
+        a,b=map(int,input().rstrip().split())
+        print(1 if find(a)==find(b) else 0)
+    print()
+        
+    
