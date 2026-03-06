680c1fff27c868b246fc7d4702940194ace51022
wonza-hub <wonza8585@gmail.com>
2026-03-06 22:35:14 +0900

    (복습) [프로그래머스] 섬 연결하기

--- Graph/신장트리/최소비용신장트리/프림(Prim)/[X]섬 연결하기.py
+++ Graph/신장트리/최소비용신장트리/프림(Prim)/[X]섬 연결하기.py
@@ -21,3 +21,29 @@ def solution(n, costs):
                     hq.heappush(q, (graph[v][i], i))
             
     return answer
+
+# 복습 풀이
+import heapq as hq
+
+def solution(n, costs):
+    answer = 0
+    ch=[0]*n
+    graph=[[0]*n for _ in range(n)]
+    q=[]
+    hq.heappush(q,(0,0))
+    
+    for s,e,cost in costs:
+        graph[s][e]=cost
+        graph[e][s]=cost
+        
+    while q:
+        c,v=hq.heappop(q)
+        if ch[v]:
+            continue
+        ch[v]=1
+        answer+=c
+        for end,cost2 in enumerate(graph[v]):
+            if not ch[end] and cost2:
+                hq.heappush(q,(cost2,end))
+    
+    return answer
