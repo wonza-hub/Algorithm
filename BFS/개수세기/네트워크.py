74cbdebc8ff5febc9884dcd1e8eb9d61487cae9f
wonza-hub <wonza8585@gmail.com>
2026-03-12 11:40:32 +0900

    (복습) [프로그래머스] 네트워크

--- BFS/개수세기/네트워크.py
+++ BFS/개수세기/네트워크.py
@@ -19,3 +19,30 @@ def solution(n, computers):
             answer += 1
 
     return answer
+
+# 복습 풀이
+# UNION-FIND로 풀이
+def find(x,p):
+    if p[x]!=x:
+        p[x]=find(p[x],p)
+    return p[x]
+    
+def union(a,b,p):
+    a=find(a,p)
+    b=find(b,p)
+    if a>b:
+        p[a]=b
+    else:
+        p[b]=a
+     
+def solution(n, computers):
+    answer = 0
+    p=[i for i in range(n)]
+    for i in range(len(computers)):
+        for j in range(len(computers[0])):
+            if i==j:
+                continue
+            if computers[i][j]==1:
+                union(i,j,p)
+                
+    return len(set(find(i,p) for i in range(n)))
